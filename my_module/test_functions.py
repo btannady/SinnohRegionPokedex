a33d9426a35c4c269772aaf1a6a252f3446bd91b
Benedict Tannady <benedicttannady@benedicts-mbp.dynamic.ucsd.edu>
2019-12-05 01:32:46 -0800

    worked on test_functions.py, got a little bit done

--- my_module/test_functions.py
+++ my_module/test_functions.py
@@ -1,35 +1,23 @@
 from my_module.functions import *
 
-"""
-Keep in mind, all the functions used are void.
-"""
-
-# tests if functions return an error
-def iserror(func, *args, **kw):
-    try:
-        func(*args, **kw)
-        return False
-    except Exception:
-        return True
-    
 
 # test the findWithName function
 def test_findWithName():
     
-    # check function doesn't throw an error
-    assert(iserror(float, 'as1f') == False)
+    assert(findWithName("pikachu") == "PikachuElectric25")        # test for all lowercase input
+    assert(findWithName("GARCHOMP") == "GarchompDragonGround445") # test for all uppercase input
+    assert(findWithName("mAgIKArP") == "MagikarpWater129")        # test for mixed-casing input
+    
+    # test invalid pokemon names
+    assert()findWithName("mrPoopyHead321") == ["The Pokémon name [myPoopyHead321] was not found in the Pokédex database..."] 
+    assert()findWithName("Harambe") == ["The Pokémon name [Harambe] was not found in the Pokédex database..."] 
+    assert()findWithName("Jake Paul") == ["The Pokémon name [Jake Paul] was not found in the Pokédex database..."] 
  
     
 # test the findWithType function
 def test_findWithType():
     
-    # test with positive int value
-    # test with negative int value
-    # test with random character
-    # test with string value
-    # test with all capitals string
-    # test with all lowercase string
-    # test with both capital&lowercase string
+
 
 
 # test the findWithNdex function
